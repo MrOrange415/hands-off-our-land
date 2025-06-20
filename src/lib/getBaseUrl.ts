26d30242ef7af6fe3c91a667c4d5592b9498cd5a
Monte Tom <montetom@mac.lan>
2025-06-20 02:57:11 -0700

    cleanup

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
   if (typeof window !== 'undefined') return ''; // use relative paths in browser
-  if (process.env.NEXT_PUBLIC_BASE_URL) return `https://${process.env.NEXT_PUBLIC_BASE_URL}`; // Vercel runtime
+  if (process.env.NEXT_PUBLIC_BASE_URL) return `https://${process.env.NEXT_PUBLIC_BASE_URL}`;
   return 'http://localhost:3000'; // local fallback
 }
