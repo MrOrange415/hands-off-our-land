487e51c93896969c4e455bb9e47b6d58f2c76aca
Monte Tom <montetom@mac.lan>
2025-06-20 01:31:41 -0700

    fix search param issue

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import ResultsClient from "./ResultsClient";
+import { Suspense } from "react";
 
 export default function ResultsPage() {
-  return <ResultsClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResultsClient />
+    </Suspense>
+  );
 } 
