933c1d55eff754b231eff935537b8c85c68aaffb
Monte Tom <montetom@mac.lan>
2025-06-28 16:21:21 -0700

    hardcode backend url again

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,14 +1,4 @@
 export function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    // In the browser, use relative path
-    return "";
-  }
-
-  // On Vercel (production or preview)
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-
-  // Local development
-  return "http://localhost:3000";
+  if (typeof window !== 'undefined') return ''; // use relative paths in browser
+  return 'https://hands-off-our-land.vercel.app';
 }
